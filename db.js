500461b8a7b1624174f980806223383030a73417
zofejes <zofejes@gmail.com>
2018-05-15 14:11:05 +0200

    fixed sequelize PG url - use static if process.env.DB is not there

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+const pgUrl = ( process.env.DATABASE_URL ? process.env.DATABASE_URL : 'postgres://gukthoqhvggrsg:621c81a1aa388a033d64c3c9e5d82658aeb0f59d1284079aeecc701c2d739f97@ec2-79-125-12-48.eu-west-1.compute.amazonaws.com:5432/d7b9alq02898m3')
+
+const sequelize = new Sequelize(pgUrl, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
