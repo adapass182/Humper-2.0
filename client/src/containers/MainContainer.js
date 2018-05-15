78deabdd23e7811af40709159afaaf7bc66c86c4
Lennart <lwijers@gmail.com>
2018-05-15 15:29:22 +0200

    new pageviews

---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,61 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import Main from './Main'
+import Test from './Test'
+import { setCurrentPage } from '../actions/setCurrentPage'
+import { connect } from 'react-redux'
+import Viewer from './Viewer'
+// import { setCurrentPage } from '../actions/setCurrentPage'
+
+
+class MainContainer extends PureComponent {
+  static propTypes = {
+    currentPage: PropTypes.string.isRequired
+  }
+
+pageview = () => {
+  if (this.props.currentPage === 'main'){
+    return <Viewer/>
+  } if (this.props.currentPage === 'test'){
+    return <Test/>
+  }
+}
+handleClick() {
+  this.props.setCurrentPage('test')
+}
+
+  render() {
+
+    return (
+      <div className="Hcontainer">
+
+        <header className="App-header">
+          <h1 className="App-title">
+            HUMPER
+          </h1>
+        </header>
+
+        <div id="leftSpace"></div>
+        {this.pageview()}
+
+        <div id="rightSpace"></div>
+        <div id="footer">
+          <button onClick={this.handleClick.bind(this)}>switch</button>
+        </div>
+
+      </div>
+
+    )}
+  }
+
+
+
+
+  const mapStateToProps = (reduxState) => {
+    return {
+      currentPage: reduxState.currentPage,
+    }
+  }
+
+
+export default connect(mapStateToProps, { setCurrentPage })(MainContainer)
