4cdfa633b1652be22c43ae4f5f669d47840578d3
Lennart <lwijers@gmail.com>
2018-05-16 14:30:33 +0200

    first styling pass, finished fog rating page

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -8,12 +8,13 @@ import RegisterForm_component from '../components/RegisterForm_component'
 import MatchesPage_component from '../components/MatchesPage_component'
 import ProfilePage_component from '../components/ProfilePage_component'
 import { login, noUser } from '../actions/users'
+// import humperIcon from "../images/humperIcon"
 
 
 class MainContainer extends PureComponent {
   constructor(props) {
     super(props)
-    this.state = { currentPage: 'main' }
+    this.state = { currentPage: 'rateADog' }
   }
 
   componentWillMount() {
@@ -22,20 +23,20 @@ class MainContainer extends PureComponent {
 
   pageview = () => {
     if (!this.props.loginSuccess && this.props.userExists) {
-      return <LoginForm_component />
+      return <LoginForm_component className="content"/>
     }
     if (!this.props.loginSuccess && !this.props.userExists) {
-      return <RegisterForm_component />
+      return <RegisterForm_component className="content" />
     }
 
-    if (this.state.currentPage === 'main') {
-      return <RateADog_component />
+    if (this.state.currentPage === 'rateADog') {
+      return <RateADog_component className="content" id="dogPic"/>
     }
     if (this.state.currentPage === 'profile') {
-      return <ProfilePage_component />
+      return <ProfilePage_component className="content" />
     }
     if (this.state.currentPage === 'matches') {
-      return <MatchesPage_component />
+      return <MatchesPage_component className="content"/>
     }
   }
 
@@ -53,25 +54,34 @@ class MainContainer extends PureComponent {
 
   render() {
     return (
-      <div className="Hcontainer">
-        <header className="App-header">
+      <div className="MainContainer">
+
+        <header className="headerContainer">
           <h1 className="App-title">Humper</h1>
+          <img  src={require("../images/humperIcon.png")} className="humperIcon"/>
         </header>
 
+
         <div id="leftSpace" />
+
+
         {this.pageview()}
 
+
         <div id="rightSpace" />
-        <div id="footer">
+
+
+
+        <div className="footer">
           {this.props.loginSuccess && (
-            <div>
-              <button name="profile" onClick={this.handleClick}>
+            <div >
+              <button className="navButton" name="profile" onClick={this.handleClick}>
                 profile
               </button>
-              <button name="main" onClick={this.handleClick}>
-                dogs
+              <button className="navButton" id="rateADogButton" name="rateADog" onClick={this.handleClick}>
+                rate a dog
               </button>
-              <button name="matches" onClick={this.handleClick}>
+              <button className="navButton"name="matches" onClick={this.handleClick}>
                 matches
               </button>
             </div>
