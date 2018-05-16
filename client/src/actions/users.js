2250ae86126f792197dbfaeac0468d1023cca12f
zofejes <zofejes@gmail.com>
2018-05-16 12:00:07 +0200

    prod build may16 morning - updated heroku baseUrl in action creators

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,6 +1,6 @@
 import * as request from 'superagent'
 
-const baseUrl = process.env.baseURL || 'http://localhost:8080'
+const baseUrl = ( process.env.BASEURL ? process.env.BASEURL : 'http://localhost:8080' )
 
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAILED = 'USER_LOGIN_FAILED'
