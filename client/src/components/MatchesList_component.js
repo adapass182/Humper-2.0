9b06f24c933760322ee0305eeeb28a4a62e967a3
adambelton <thelastgreatcynic@gmail.com>
2018-05-16 16:27:17 +0200

    fix import warnings

--- client/src/components/MatchesList_component.js
+++ client/src/components/MatchesList_component.js
@@ -1,22 +1,18 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 
 class MatchesList extends PureComponent {
-
-	render() {
-		return (
+  render() {
+    return (
       <li className="Matches">
         <p>TopDogs!</p>
       </li>
-		)
-	}
+    )
+  }
 }
 
-const mapStateToProps = function (state) {
-	return {
-
-	}
+const mapStateToProps = function(state) {
+  return {}
 }
 
 export default connect(mapStateToProps, {})(MatchesList)
--- client/src/components/MatchesPage_component.js
+++ client/src/components/MatchesPage_component.js
@@ -1,25 +1,19 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-
-import MatchesList from './MatchesList_component'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 
 class MatchesPage extends PureComponent {
-
-	render() {
-		return (
-			<div>
-				<h1>Your matches</h1>
-      <h2>Matches page!</h2>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>Your matches</h1>
+        <h2>Matches page!</h2>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = function (state) {
-	return {
-
-	}
+const mapStateToProps = function(state) {
+  return {}
 }
 
 export default connect(mapStateToProps, {})(MatchesPage)
