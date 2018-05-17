942b630bd0f391f607e4d728ce522ee7c0acf5c3
adapass182 <adampassingham90@gmail.com>
2018-05-17 17:23:19 +0200

    logging out now resets the matches state

--- client/src/reducers/matches_reducer.js
+++ client/src/reducers/matches_reducer.js
@@ -1,9 +1,12 @@
 import { MATCHES_FETCHED } from '../actions/matches'
+import { USER_LOGOUT_SUCCESS } from '../actions/users'
 
 export default function(state = {}, action) {
   switch (action.type) {
     case MATCHES_FETCHED:
       return action.payload
+    case USER_LOGOUT_SUCCESS:
+      return {}
     default:
       return state
   }
