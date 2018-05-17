4dd477b62d4d0cf9f38bde16f89985ebbf51b34c
adambelton <thelastgreatcynic@gmail.com>
2018-05-17 15:40:37 +0200

    tweaked admin dashboard

--- client/src/components/AdminDashboard_component.js
+++ client/src/components/AdminDashboard_component.js
@@ -2,14 +2,18 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { getUserStats } from '../actions/users'
 import { getDogStats } from '../actions/rateDog'
-import { getTopTenLiked, getTopTenDisliked, getMostActiveUsers} from '../actions/matches'
+import {
+  getTopTenLiked,
+  getTopTenDisliked,
+  getMostActiveUsers
+} from '../actions/matches'
 
 class AdminDashboard extends PureComponent {
-  constructor(){
+  constructor() {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
-      stats: "liked"
+      stats: 'liked'
     }
   }
 
@@ -18,13 +22,13 @@ class AdminDashboard extends PureComponent {
     this.setState({
       stats: e.target.value
     })
-    if (e.target.value === "liked") {
+    if (e.target.value === 'liked') {
       this.props.getTopTenLiked()
     }
-    if (e.target.value === "disliked") {
+    if (e.target.value === 'disliked') {
       this.props.getTopTenDisliked()
     }
-    if (e.target.value === "mostactive") {
+    if (e.target.value === 'mostactive') {
       this.props.getMostActiveUsers()
     }
   }
@@ -43,87 +47,78 @@ class AdminDashboard extends PureComponent {
     return (
       <div className="dashboardContainer">
         <h2>Admin Dashboard</h2>
-          <p>Total Users: {this.props.adminStats.totalUsers}</p>
-          <p>Total Dogs Liked: {this.props.adminStats.totalDogs}</p>
-
+        <p>Total Users: {this.props.adminStats.totalUsers}</p>
+        <p>Total Dogs Liked: {this.props.adminStats.totalDogs}</p>
 
         <form onSubmit={this.handleSubmit}>
-          <select id="adminDropDown"  name="stats" onChange={this.handleSubmit}>
+          <select id="adminDropDown" name="stats" onChange={this.handleSubmit}>
             <option value="liked">Top ten liked breeds</option>
             <option value="disliked">Top ten disliked breeds</option>
             <option value="mostactive">Most active users</option>
           </select>
         </form>
 
-
-          { this.state.stats === "liked" &&
-              <div>
-              <p>Top ten liked breeds: </p>
-              <table>
-                <tbody>
+        {this.state.stats === 'liked' && (
+          <div>
+            <table>
+              <tbody>
                 <tr>
                   <th>Breed</th>
                   <th>Likes</th>
                 </tr>
-                { admin.topTenLike &&
-                    admin.topTenLike.map((e,i) => (
-                      <tr key={i}>
-                        <td>{e.breed}</td>
-                        <td>{e.count}</td>
-                      </tr>
-                    ))
-                }
-                </tbody>
-              </table>
-          </div>
-        }
-
-        { this.state.stats === "disliked" &&
-            <div>
-            <p>Top ten disliked breeds: </p>
-            <table>
-              <tbody>
-              <tr>
-                <th>Breed</th>
-                <th>Dislikes</th>
-              </tr>
-              { admin.topTenDislike &&
-                  admin.topTenDislike.map((e,i) => (
+                {admin.topTenLike &&
+                  admin.topTenLike.map((e, i) => (
                     <tr key={i}>
                       <td>{e.breed}</td>
                       <td>{e.count}</td>
                     </tr>
-                  ))
-              }
-            </tbody>
-          </table>
-        </div>
-        }
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
 
-        { this.state.stats === "mostactive" &&
-            <div>
-            <p>Most active users:</p>
+        {this.state.stats === 'disliked' && (
+          <div>
             <table>
               <tbody>
-              <tr>
-                <th>Name</th>
-                <th>e-mail</th>
-                <th>likes</th>
-              </tr>
-              { admin.mostActive &&
-                  admin.mostActive.map((e,i) => (
+                <tr>
+                  <th>Breed</th>
+                  <th>Dislikes</th>
+                </tr>
+                {admin.topTenDislike &&
+                  admin.topTenDislike.map((e, i) => (
+                    <tr key={i}>
+                      <td>{e.breed}</td>
+                      <td>{e.count}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {this.state.stats === 'mostactive' && (
+          <div>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>e-mail</th>
+                  <th>likes</th>
+                </tr>
+                {admin.mostActive &&
+                  admin.mostActive.map((e, i) => (
                     <tr key={i}>
                       <td>{e.firstname}</td>
                       <td>{e.email}</td>
                       <td>{e.ct}</td>
                     </tr>
-                  ))
-              }
-            </tbody>
-          </table>
-        </div>
-        }
-
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     )
   }
@@ -133,6 +128,10 @@ const mapStateToProps = ({ adminStats }) => {
   return { adminStats }
 }
 
-export default connect(mapStateToProps, { getUserStats, getDogStats, getTopTenLiked, getTopTenDisliked, getMostActiveUsers})(
-  AdminDashboard
-)
+export default connect(mapStateToProps, {
+  getUserStats,
+  getDogStats,
+  getTopTenLiked,
+  getTopTenDisliked,
+  getMostActiveUsers
+})(AdminDashboard)
