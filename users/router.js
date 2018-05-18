45c8bfd2e7ff1b6356ce1123bc5b32c20e453db8
zofejes <zofejes@gmail.com>
2018-05-18 13:19:42 +0200

    custom email sent on new user registration

--- users/router.js
+++ users/router.js
@@ -45,8 +45,64 @@ router.post('/users', (req, res) => {
   const mailOptions = {
   from: 'humperapp@gmail.com', // sender address
   to: user.email, // list of receivers
-  subject: 'Signup to Humper!', // Subject line
-  html: `<p>Thanks for signing up, ${user.firstname} </p>`// plain text body
+  subject: 'Welcome to Humper!', // Subject line
+    html: `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <style media="screen" type="text/css">
+  .main {
+    position: relative;
+    text-align: center;
+    width: 360px;
+    height: 640px;
+    margin: auto;
+  }
+  .header {
+    display: flex;
+    align-items: center;
+    background-color: white;
+    height: 100px;
+    padding: 0;
+    color: red;
+    margin: 0;
+    justify-content: center;
+  }
+  .title {
+    padding: auto;
+    font-family: sans-serif;
+    font-size: 36px;
+    color: red;
+    font-weight: bold; 
+    font-style: italic;
+  }
+  .humperIcon {
+    max-height: 80px;
+    max-width: auto;
+    margin: auto;
+  }
+  .message { 
+    font-family: sans-serif
+  }
+  .larger-text {
+    font-size: 1.2em;
+  }
+  </style>
+</head>
+<body>
+  <div class="main">
+    <div class="header">
+      <img class="humperIcon" src="https://i.imgur.com/z6qPgRx.png">
+    </div>
+    <div class="message">
+      <p class="title">Humper</p>
+      <p class="larger-text">Thank you for signing up, ${user.firstname}!<p>
+      <p>You can now start liking dogs to get matched to other people with similar dog breed preferences.<p>
+      <p class="larger-text">Happy Humping!<p>
+    </div>
+  </div>
+</body>
+</html>`// plain text body
   }
 
   User.create(user)
