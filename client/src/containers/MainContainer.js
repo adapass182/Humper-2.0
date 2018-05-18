df506e50e6667db1c753196e9798a943aba8407a
adapass182 <adampassingham90@gmail.com>
2018-05-18 11:32:49 +0200

    removed autologin in mainContainer, removed consolelog in one of the reducers

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -16,10 +16,6 @@ class MainContainer extends PureComponent {
     this.state = { currentPage: 'rateADog' }
   }
   
-  //  componentWillMount() {
-  //    this.props.login('secondUser@humper.com', 'password')
-  //  }
-
   pageview = () => {
     if (!this.props.loginSuccess && this.props.userExists) {
       return <LoginForm className="content" />
--- client/src/reducers/registerFailed_reducer.js
+++ client/src/reducers/registerFailed_reducer.js
@@ -3,7 +3,6 @@ import { USER_REGISTER_FAILED, USER_LOGOUT_SUCCESS } from '../actions/users'
 export default function(state = null, action) {
   switch (action.type) {
     case USER_REGISTER_FAILED:
-      console.log('Reducer: ', action.payload)
       return {
         error: action.payload
       }
